df32fd0ae6f4d6028834266091bc5f56ae56002a
Komal Shinde <komalshinde8299@gmail.com>
2023-05-26 13:42:14 +0530

    Added:Code for calculating wage

--- src/com/first/employee_wage/Employee_Wage.java
+++ src/com/first/employee_wage/Employee_Wage.java
@@ -4,16 +4,20 @@ import java.util.Random;
 
 public class Employee_Wage {
     public static void main(String[] args) {
-        System.out.println("* Welcome to Employee Wage Computation Program *");
+        int empHr = 0;
+        int ratePerHr = 20;
         Random random = new Random();
         int random_num = random.nextInt(2);
         System.out.println("Random no genrated is : " +random_num);
         if(random_num == 0){
+            empHr = 0;
             System.out.println("Employee is absent");
         } else if (random_num == 1) {
             System.out.println("Employee is present");
+            empHr = 8;
 
         }
+        System.out.println("Daily employee wage is : " + empHr * ratePerHr);
 
 
     }
