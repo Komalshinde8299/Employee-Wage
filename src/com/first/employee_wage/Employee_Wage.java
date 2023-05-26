a76d7afb674ebd69b64d6667abe200d91cd67198
Komal Shinde <komalshinde8299@gmail.com>
2023-05-26 12:53:34 +0530

    Added:Code to check employee is present or absent

--- src/com/first/employee_wage/Employee_Wage.java
+++ src/com/first/employee_wage/Employee_Wage.java
@@ -1,7 +1,20 @@
 package com.first.employee_wage;
 
+import java.util.Random;
+
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("* Welcome to Employee Wage Computation Program *");
+        Random random = new Random();
+        int random_num = random.nextInt(2);
+        System.out.println("Random no genrated is : " +random_num);
+        if(random_num == 0){
+            System.out.println("Employee is absent");
+        } else if (random_num == 1) {
+            System.out.println("Employee is present");
+
+        }
+
+
     }
 }
