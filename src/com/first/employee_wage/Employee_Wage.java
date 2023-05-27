60325055484b0d39eecc2c5032b595888c9600f7
Komal Shinde <komalshinde8299@gmail.com>
2023-05-27 11:57:47 +0530

    Added:Code to calculate 20 days wage

--- src/com/first/employee_wage/Employee_Wage.java
+++ src/com/first/employee_wage/Employee_Wage.java
@@ -4,25 +4,27 @@ import java.util.Random;
 
 public class Employee_Wage {
     public static void main(String[] args) {
+        int totalSalary = 0;
+        int salary = 0;
         int empHr = 0;
         int ratePerHr = 20;
-        Random random = new Random();
-        int random_num = random.nextInt(3);
-        System.out.println("Random no genrated is : " +random_num);
 
-        switch (random_num){
-            case 1 :
+        for(int i = 0; i<=20; i++ ) {
+            Random random = new Random();
+            int random_num = random.nextInt(3);
+
+            if (random_num == 1) {
                 empHr = 8;
-                System.out.println("Employee is present full time");
-                break;
-            case 2 :
-                System.out.println("Employee is present part time");
+
+            } else if (random_num == 2) {
                 empHr = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
+            } else {
+                empHr = 0;
+            }
+            salary = empHr * ratePerHr;
+            totalSalary = salary + totalSalary;
         }
-        System.out.println("Daily employee wage is : " + empHr * ratePerHr);
+        System.out.println("20 days employee wage is : " + totalSalary);
 
     }
 }
