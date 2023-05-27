01ff5bfeb8cbe99eeb9e4e7afda7aadea60272d3
Komal Shinde <komalshinde8299@gmail.com>
2023-05-27 11:06:34 +0530

    Added:Code for part time or full time work

--- src/com/first/employee_wage/Employee_Wage.java
+++ src/com/first/employee_wage/Employee_Wage.java
@@ -7,15 +7,16 @@ public class Employee_Wage {
         int empHr = 0;
         int ratePerHr = 20;
         Random random = new Random();
-        int random_num = random.nextInt(2);
+        int random_num = random.nextInt(3);
         System.out.println("Random no genrated is : " +random_num);
-        if(random_num == 0){
-            empHr = 0;
-            System.out.println("Employee is absent");
-        } else if (random_num == 1) {
-            System.out.println("Employee is present");
+        if(random_num == 1){
             empHr = 8;
-
+            System.out.println("Employee is present full time");
+        } else if (random_num == 2) {
+            System.out.println("Employee is present part time");
+            empHr = 8;
+        } else {
+            System.out.println("Employee is absent");
         }
         System.out.println("Daily employee wage is : " + empHr * ratePerHr);
 
