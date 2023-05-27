bb1ccb6ba2b4ceb98719705f2c8967942507378c
Komal Shinde <komalshinde8299@gmail.com>
2023-05-27 12:50:40 +0530

    Added:Code to calculate wage till reach limit of hrs or days

--- src/com/first/employee_wage/Employee_Wage.java
+++ src/com/first/employee_wage/Employee_Wage.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 public class Employee_Wage {
     public static void main(String[] args) {
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
         int totalSalary = 0;
         int salary = 0;
         int empHr = 0;
         int ratePerHr = 20;
 
-        for(int i = 0; i<=20; i++ ) {
+            while(totalEmpHrs < 100 || totalWorkingDays < 20){
             Random random = new Random();
             int random_num = random.nextInt(3);
 
@@ -21,9 +23,14 @@ public class Employee_Wage {
             } else {
                 empHr = 0;
             }
+            totalWorkingDays++;
+            totalEmpHrs = empHr + totalEmpHrs;
+
             salary = empHr * ratePerHr;
             totalSalary = salary + totalSalary;
         }
+        System.out.println("Total working days : " + totalWorkingDays);
+        System.out.println("Total working Hrs : " + totalEmpHrs);
         System.out.println("20 days employee wage is : " + totalSalary);
 
     }
