2656311a66b252e6ba58a23a2a6c7efcd5c12580
Komal Shinde <komalshinde8299@gmail.com>
2023-05-27 11:23:35 +0530

    Added:Code to calculate wage by switch case

--- src/com/first/employee_wage/Employee_Wage.java
+++ src/com/first/employee_wage/Employee_Wage.java
@@ -9,17 +9,20 @@ public class Employee_Wage {
         Random random = new Random();
         int random_num = random.nextInt(3);
         System.out.println("Random no genrated is : " +random_num);
-        if(random_num == 1){
-            empHr = 8;
-            System.out.println("Employee is present full time");
-        } else if (random_num == 2) {
-            System.out.println("Employee is present part time");
-            empHr = 4;
-        } else {
-            System.out.println("Employee is absent");
+
+        switch (random_num){
+            case 1 :
+                empHr = 8;
+                System.out.println("Employee is present full time");
+                break;
+            case 2 :
+                System.out.println("Employee is present part time");
+                empHr = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
         System.out.println("Daily employee wage is : " + empHr * ratePerHr);
-
 
     }
 }
